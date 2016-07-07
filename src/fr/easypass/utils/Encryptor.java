d67c2b3009c02eb27e11f1a5d53c50e19c8290cc
aturcey <aturcey@users.noreply.github.com>
2016-07-07 19:23:02 +0200

    Add encryptor password configuration

--- src/fr/easypass/utils/Encryptor.java
+++ src/fr/easypass/utils/Encryptor.java
@@ -1,19 +1,32 @@
 package fr.easypass.utils;
 
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 import org.jasypt.util.text.StrongTextEncryptor;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import fr.easypass.manager.ConnectorManager;
 
 public class Encryptor {
     
     String encryptedPassword;
     StrongTextEncryptor textEncryptor = new StrongTextEncryptor();
+    String password;
     
     public Encryptor() {
+        
+        InputStream stream = ConnectorManager.class.getClassLoader().getResourceAsStream("config.properties");
+        JsonObject jsonObject = new JsonParser().parse(new InputStreamReader(stream)).getAsJsonObject();
+        this.password = jsonObject.get("encryptor").getAsJsonObject().get("password").getAsString();
         
     }
     
     public void setPlainPassword(String plainPassword){
         
-        textEncryptor.setPassword("password...");
+        textEncryptor.setPassword(this.password);
         this.encryptedPassword = textEncryptor.encrypt(plainPassword);
         
     }
@@ -26,7 +39,7 @@ public class Encryptor {
     
     public String decryptPassword(String password){
         
-        textEncryptor.setPassword("password...");
+        textEncryptor.setPassword(this.password);
         return textEncryptor.decrypt(password);
         
     }
